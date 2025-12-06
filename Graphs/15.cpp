3058f71344489bc4aba261bbf88618013a7a7a7e
Shreyas Patil <shreyaspatil9131@gmail.com>
2025-12-06 19:47:24 +0530

    d

---
+++ Graphs/15.cpp
@@ -0,0 +1,80 @@
+// Topological Sort — DFS and Kahn's Algorithm
+
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <limits>
+#include <climits>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <numeric>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution_DFS {
+public:
+    void dfs(int node, vector<int> &vis, vector<vector<int>> &adj, stack<int> &st) {
+        vis[node] = 1;
+        for (int it : adj[node]) {
+            if (!vis[it]) dfs(it, vis, adj, st);
+        }
+        st.push(node);
+    }
+
+    vector<int> topoSort(int n, vector<vector<int>>& edges) {
+        vector<int> vis(n, 0);
+        vector<vector<int>> adj(n);
+        stack<int> st;
+
+        for (auto &e : edges) {
+            int a = e[0], b = e[1];
+            adj[a].push_back(b);
+        }
+
+        for (int i = 0; i < n; ++i) {
+            if (!vis[i]) dfs(i, vis, adj, st);
+        }
+
+        vector<int> ans;
+        while (!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        return ans;
+    }
+};
+
+class Solution_Kahn {
+public:
+    vector<int> topoSort(int n, vector<vector<int>>& edges) {
+        vector<int> indeg(n, 0);
+        vector<vector<int>> adj(n);
+        for (auto &e : edges) {
+            int a = e[0], b = e[1];
+            adj[a].push_back(b);
+            indeg[b]++;
+        }
+
+        queue<int> q;
+        for (int i = 0; i < n; ++i) if (indeg[i] == 0) q.push(i);
+
+        vector<int> ans;
+        while (!q.empty()) {
+            int node = q.front(); q.pop();
+            ans.push_back(node);
+            for (int it : adj[node]) {
+                if (--indeg[it] == 0) q.push(it);
+            }
+        }
+        return ans;
+    }
+};
---
+++ Graphs/18.cpp
@@ -0,0 +1,54 @@
+// Course schedule 2
+
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <limits>
+#include <climits>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <numeric>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> topoSort(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(n);
+        vector<int> indeg(n, 0);
+
+        for (auto &e : edges) {
+            int u = e[0], v = e[1];
+            // edges are given as [u, v] meaning "to take u you must first take v"
+            adj[v].push_back(u);
+            indeg[u]++;
+        }
+
+        queue<int> q;
+        for (int i = 0; i < n; ++i) if (indeg[i] == 0) q.push(i);
+
+        vector<int> ans;
+        while (!q.empty()) {
+            int node = q.front(); q.pop();
+            ans.push_back(node);
+            for (int nb : adj[node]) {
+                if (--indeg[nb] == 0) q.push(nb);
+            }
+        }
+        return ans;
+    }
+
+    vector<int> findOrder(int n, vector<vector<int>>& edges) {
+        vector<int> topo = topoSort(n, edges);
+        return topo.size() == n ? topo : vector<int>{};
+    }
+};
